b35bd6bb5463b835548a63d25a5b2f07c395ba2a
ahmed barkhed <davidmohmed@gmail.com>
2025-03-01 01:20:13 +0400

    Re-linked project to GitHub

--- app/loading.js
+++ app/loading.js
@@ -1,21 +1,9 @@
-"use client";
-import Image from "next/image";
-import { BarLoader } from "react-spinners";
+import LoadingComponent from "./components/loading";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center h-[80vh] bg-white">
-      {/* الشعار */}
-      <Image
-        src="/images/Logo.png" 
-        alt="Loading..."
-        width={150} 
-        height={150}
-        className="mb-4"
-      />
-      
-      {/* الـ Loader */}
-      <BarLoader color="#184b72" width={150} />
-    </div>
+    <>
+    <LoadingComponent />
+    </>
   );
 }
