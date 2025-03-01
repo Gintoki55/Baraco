d099287ea8682e34772f6d5f47cd87f08681d57c
ahmed barkhed <davidmohmed@gmail.com>
2025-03-01 15:47:27 +0400

    change in config js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    experimental: {
+    serverActions: {}, // يجب أن يكون كائنًا فارغًا بدلًا من true
+  },
 };
 
 export default nextConfig;
