f3012ef59ee7896ac64aee4efb1ac9314f906258
ahmed barkhed <davidmohmed@gmail.com>
2025-03-05 11:29:26 +0400

    fix some problem link to sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://barkhad-group.com', // رابط موقعك
+  generateRobotsTxt: true, // إنشاء ملف robots.txt تلقائياً
+  sitemapSize: 5000, // الحد الأقصى لعدد الروابط في كل خريطة موقع
+  generateIndexSitemap: false, // إذا كنت لا تريد استخدام فهرس لخريطة الموقع
+};
